a12ca1dc12dfb32ab1de7d434e99572193bba39b
Abhijeet Kokat <118294497+Abhijeetkokat007@users.noreply.github.com>
2025-12-24 15:21:04 +0530

    added small change in send tokan file

--- server/utils/sendToken.js
+++ server/utils/sendToken.js
@@ -1,17 +1,20 @@
 export const sendToken = (user, statusCode, message, res) => {
   const token = user.generateToken();
+
   res
     .status(statusCode)
     .cookie("token", token, {
+      httpOnly: true,
+      secure: true,        // REQUIRED (HTTPS / Render)
+      sameSite: "none",    // REQUIRED (cross-origin)
+      path: "/",           // IMPORTANT
       expires: new Date(
         Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
       ),
-      httpOnly: true,
     })
     .json({
       success: true,
       user,
       message,
-      token,
     });
 };
