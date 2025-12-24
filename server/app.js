6f497b7f7de55db47d13f5f855714fc433b229c1
Abhijeet Kokat <118294497+Abhijeetkokat007@users.noreply.github.com>
2025-12-24 14:38:11 +0530

    added small changes

--- server/app.js
+++ server/app.js
@@ -18,7 +18,6 @@ config({ path: "./config.env" });
 const __dirname = path.resolve();
 
 app.use(cookieParser());
-app.use(express.json());
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ limit: "100mb", extended: true }));
 const allowedOrigins = [
